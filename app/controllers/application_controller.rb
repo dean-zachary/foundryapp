21f042628daf13029bf347cb622b96ba941679e6
dean_zachary <dean_zachary@live.com>
2016-05-07 13:39:40 -0700

    Added contact page with email and message request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+def thank_you
+	@name = params[:name]
+	@email = params[:email]
+	@message = params[:message]
+	ActionMailer::Base.mail(:from => @email, :to => 'dean_zachary@live.com', :subject => "A new contact form message from #{@name}", :body => @message). deliver_now
+end
