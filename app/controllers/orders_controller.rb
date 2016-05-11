bf33acbaca32f328167e84b69625db00d95b0082
dean_zachary <dean_zachary@live.com>
2016-05-11 16:09:03 -0700

    added authorization using cancancan and a warning message

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!
+	before_filter :authenticate_user!
+
 	def index
 	end
 
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,5 +4,6 @@ class Ability
   def initialize(user)
     user ||= User.new # geust user (not logged in)
     can :manage, User, id: user.id
+    end
   end
-end
+
