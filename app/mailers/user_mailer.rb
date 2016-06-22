2703489768ab48d92d22c25cbe2f5744f816de21
dean_zachary <dean_zachary@live.com>
2016-06-22 12:32:49 -0700

    bug fixing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,6 @@
 class UserMailer < ApplicationMailer
   default from: "dean_zachary@live.com"
 
-  def registration_confimation(user)
-  	@user = user
-  	mail(:to => "{#user.name} <#{user.email}>", :subject => "Registration Confirmation")
-  end
-  
   def contact_form(email, name, message)
   @message = message
     mail(:from => email,
