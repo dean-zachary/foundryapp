daf7520cefa4b748ff7819463426ea2b0fa21316
dean_zachary <dean_zachary@live.com>
2016-06-22 12:17:59 -0700

    email error2

--- db/migrate/20160622180730_add_confirmable_to_devise.rb
+++ db/migrate/20160622180730_add_confirmable_to_devise.rb
@@ -5,7 +5,7 @@ class AddConfirmableToDevise < ActiveRecord::Migration
   	add_column :users, :confirmation_sent_at, :datetime
   	add_index :users, :confirmation_token, unique: true
 
-    execute("UPDATE users SET confirmed_at = :date('now)")
+    User.all.update_all confirmed_at: Time.now
 
     end
 
