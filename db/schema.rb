ffe612dfd65e8476267fb5ac0c40f28cf7ed2157
dean_zachary <dean_zachary@live.com>
2016-07-10 17:24:07 -0700

    set up stripe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160622180730) do
+ActiveRecord::Schema.define(version: 20160710221807) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20160622180730) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
-    t.string   "price"
+    t.decimal  "price"
   end
 
   create_table "users", force: :cascade do |t|
